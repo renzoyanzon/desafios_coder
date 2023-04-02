d31a0bbc4589f6a911227f7d63b596fba4c2f35e
Renzo Yanzon <renzoyanzon@gmail.com>
2023-04-02 10:24:54 -0300

    desafio logs,gzio - profiling

--- src/routes/info/info.routes.js
+++ src/routes/info/info.routes.js
@@ -4,6 +4,7 @@ const DataInfo = require('../../services/info/info.services')
 const dataInfo = new DataInfo();
 
 router.get('/',async (_req,res)=>{
+    //console.log( await dataInfo.getInfoProcess())
     res.status(200).json({
         success: true,
         data: await dataInfo.getInfoProcess()
