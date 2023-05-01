ee63ea543b3a8e8e938bf5b9d93a06128887534c
Renzo Yanzon <renzoyanzon@gmail.com>
2023-05-01 19:34:22 -0300

    test APIREST

---
+++ src/tests/users/users.test.js
@@ -0,0 +1,24 @@
+const assert = require('assert').strict;
+
+describe('test de creacion de usuario', function(){
+
+    before(function(){
+        console.info('\n ***** Comienzo total del test *****');
+    });
+
+    after(function(){
+        console.info('\n ***** Final total del test *****');
+    });
+
+    beforeEach(function(){
+        console.info('\n ***** Comienzo test individual *****');
+    });
+
+    afterEach(function(){
+        console.info('\n ***** Final test individual *****');
+    });
+
+    it('Creacion de usuario', async function(){
+
+    })
+})
